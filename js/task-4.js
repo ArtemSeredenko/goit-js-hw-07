995151a20002be39c616e17f5dd1c3588a3319ef
ArtemSeredenko <mpdart2013@gmail.com>
2024-03-17 19:58:11 +0200

    add 4 task

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const formData = {};
+
+  for (const element of form.elements) {
+    if (element.tagName === 'INPUT' && element.value.trim() === '') {
+      alert('All form fields must be filled in');
+      return;
+    }
+
+    if (element.tagName === 'INPUT') {
+      formData[element.name] = element.value.trim();
+    }
+  }
+  console.log(formData);
+  form.reset();
+});
