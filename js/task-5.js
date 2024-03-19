e4dc9f46e2da18bb93745d01f83295e103aa3573
ArtemSeredenko <mpdart2013@gmail.com>
2024-03-19 17:44:49 +0200

    add 5- 6 tasks

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+
+function changeBodyBg(event) {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  span.textContent = newColor;
+}
+
+button.addEventListener('click', changeBodyBg);
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,38 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input[type="number"]');
+const createButton = document.querySelector('button[data-create]');
+const destroyButton = document.querySelector('button[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+  const elements = [];
+  let size = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+    const div = document.createElement('div');
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    size += 10;
+    elements.push(div);
+  }
+  boxesContainer.append(...elements);
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+
+createButton.addEventListener('click', () => {
+  const amount = Number(input.value);
+  if (amount >= 1 && amount <= 100) {
+    destroyBoxes();
+    createBoxes(amount);
+  }
+  input.value = '';
+});
+
+destroyButton.addEventListener('click', destroyBoxes);
